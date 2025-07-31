8edf31cb26a266f17c800a380d1c3e0f78593f71
xifOO <wedyi28111@gmail.com>
2025-08-01 02:04:28 +0300

    feat: add list_commits method to fetch commits

--- diffetl/extract/repository.py
+++ diffetl/extract/repository.py
@@ -1,6 +1,9 @@
+from typing import List
 from diffetl.extract._repository import GitClient
-from diffetl.config import save_repo_dir
+from diffetl.config import get_repo_dir
 from git import Repo
+from git.objects import Commit as GitCommit
+from diffetl.transform.commit import Commit
 
 
 class GitHubClient(GitClient):
@@ -10,6 +13,25 @@ class GitHubClient(GitClient):
         self.repo = None
 
     def _clone(self):
-        self.repo = Repo.clone_from(self.repo_url, save_repo_dir)
+        repo_dir = get_repo_dir(self.repo_url)
+
+        if repo_dir.exists():
+            self.repo = Repo(str(repo_dir))
+            if not self.repo.is_dirty():
+                origin = self.repo.remotes.origin
+                origin.pull()
+        else:
+            repo_dir.parent.mkdir(parents=True, exist_ok=True)
+            self.repo = Repo.clone_from(self.repo_url, str(repo_dir))
     
+    def list_commits(self, count: int) -> List[Commit]:
+        self._clone()
+        if not self.repo:
+            raise RuntimeError("Init repo failed.")
+
+        git_commits: List[GitCommit] = list(self.repo.iter_commits("master", max_count=count))
+        return [Commit.from_git_commit(gc) for gc in git_commits]
+
+
     
+
