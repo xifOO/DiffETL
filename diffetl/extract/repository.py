b356232ac3ea7d018752b082df3270a747cccfd9
xifOO <wedyi28111@gmail.com>
2025-09-08 19:13:31 +0300

    refactor: rewrite LocalGitRepository to use RawCommitsBatch

--- diffetl/extract/repository.py
+++ diffetl/extract/repository.py
@@ -1,33 +1,33 @@
-from typing import Dict, List
-
-from git import Commit as GitCommit
+from datetime import datetime
+from typing import Optional
+from uuid import uuid4
 
 from diffetl.extract._client import GitClient
-from diffetl.extract._repository import GitRepository
-from diffetl.transform.commit import Commit, CommitElement
-from diffetl.transform.diff import Diff
+from diffetl.extract._raw import SourceInfo
+from diffetl.extract.batch import RawCommitsBatch
 
 
-class LocalGitRepository(GitRepository):
+class LocalGitRepository:
     def __init__(self, git_client: GitClient):
         self.git_client = git_client
 
-    def fetch_commits(self, max_count: int, branch: str) -> Dict[str, CommitElement]:
-        raw_commits: List[GitCommit] = self.git_client.list_commits(max_count, branch)
-        elements = self._create_linked_elements(raw_commits)
-        return CommitElement.to_group_dict(elements)
-
-    def _create_linked_elements(
-        self, raw_commits: List[GitCommit]
-    ) -> List[CommitElement]:
-        elements: List[CommitElement] = []
-
-        for raw_commit in raw_commits:
-            commit = Commit.from_git_commit(raw_commit)
-            diff = Diff.to_diff(raw_commit)
-
-            element = CommitElement(commit=commit, diff=diff)
-
-            elements.append(element)
-
-        return elements
+    def extract_commits_batch(
+        self, batch_size: int, branch: str, last_sha: Optional[str] = None
+    ):
+        raw_commits_batch = RawCommitsBatch(
+            load_id=uuid4(),
+            load_timestamp=datetime.now(),
+            source_info=SourceInfo(repo_url=self.git_client.repo_url, branch=branch),
+        )
+        try:
+            list_raw_commits, new_last_sha = self.git_client.list_commits(
+                batch_size, branch, last_sha=last_sha
+            )
+            raw_commits_batch.add_commits(list_raw_commits)
+            raw_commits_batch.validate(
+                expected=batch_size, actual=len(list_raw_commits)
+            )
+            return raw_commits_batch, new_last_sha
+        except Exception as e:
+            raw_commits_batch.mark_failed(str(e))
+            return raw_commits_batch, last_sha
