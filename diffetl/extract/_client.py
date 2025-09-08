d41c5c5f320c9fdaa234fbd4a2bfd7a237c3e5f8
xifOO <wedyi28111@gmail.com>
2025-09-08 19:12:52 +0300

    refactor: update GitClient method signatures and return types

--- diffetl/extract/_client.py
+++ diffetl/extract/_client.py
@@ -1,20 +1,18 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from git import Commit as GitCommit
 
 
 class GitClient(ABC):
     @abstractmethod
-    def __init__(self, repo_url: str): ...
+    def __init__(self, repo_url: str):
+        self.repo_url = repo_url
 
     @abstractmethod
     def _clone(self): ...
 
     @abstractmethod
-    def list_commits(self, count: int, branch: str) -> List[GitCommit]: ...
-
-
-class APIClientInterface(ABC):
-    @abstractmethod
-    def __init__(self, repo_url: str, token: Optional[str] = None) -> None: ...
+    def list_commits(
+        self, batch_size: int, branch: str, **kwargs
+    ) -> Tuple[List[GitCommit], Optional[str]]: ...
